ae067bb2431de3fde13d21fff5350e4126c2cb0f
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-26 04:09:40 +0500

    trending component jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+
 import Netflix from "./Netflix";
+import Trending from "./Trending";
 
 function App() {
   return (
     <div className="app-container">
       <Netflix />
+      <Trending />
     </div>
   );
 }
---
+++ src/Trending/index.jsx
@@ -0,0 +1,11 @@
+import "./Trending.css";
+
+function Trending() {
+  return (
+    <div className="trending">
+      <div className="trending-container"></div>
+    </div>
+  );
+}
+
+export default Trending;
