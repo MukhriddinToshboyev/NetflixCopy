183315c6449c0244d621556eab7107674d7b81da
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-26 01:00:23 +0500

    netflix compononts name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Netflix from "./Netflix";
 
 function App() {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <Netflix />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Netflix/index.jsx
@@ -0,0 +1,11 @@
+import "./Netflix.css";
+
+function Netflix() {
+  return (
+    <div className="header">
+      <div className="header-container">header.</div>
+    </div>
+  );
+}
+
+export default Netflix;
