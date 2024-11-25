b5a91a11d7491ac6b35ce614a6d4fca459be6a19
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-26 04:07:13 +0500

    netflix component css

--- src/Netflix/index.jsx
+++ src/Netflix/index.jsx
@@ -19,9 +19,28 @@ function Netflix() {
             </g>
           </svg>
           <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              role="img"
+              viewBox="0 0 16 16"
+              width="16"
+              height="16"
+              data-icon="LanguagesSmall"
+              aria-hidden="true"
+              className="netflix-header__svg"
+            >
+              <g fill="#ffff">
+                <path d="M10.7668 5.33333L10.5038 5.99715L9.33974 8.9355L8.76866 10.377L7.33333 14H9.10751L9.83505 12.0326H13.4217L14.162 14H16L12.5665 5.33333H10.8278H10.7668ZM10.6186 9.93479L10.3839 10.5632H11.1036H12.8856L11.6348 7.2136L10.6186 9.93479ZM9.52722 4.84224C9.55393 4.77481 9.58574 4.71045 9.62211 4.64954H6.41909V2H4.926V4.64954H0.540802V5.99715H4.31466C3.35062 7.79015 1.75173 9.51463 0 10.4283C0.329184 10.7138 0.811203 11.2391 1.04633 11.5931C2.55118 10.6795 3.90318 9.22912 4.926 7.57316V12.6667H6.41909V7.51606C6.81951 8.15256 7.26748 8.76169 7.7521 9.32292L8.31996 7.88955C7.80191 7.29052 7.34631 6.64699 6.9834 5.99715H9.06968L9.52722 4.84224Z"></path>
+              </g>
+            </svg>
             <select className="netflix-header__select">
-              <option value="English">English</option>
-              <option value="Русский">Русский</option>
+              <option className="netflix-header__select-text" value="English">
+                English
+              </option>
+              <option className="netflix-header__select-text" value="Русский">
+                Русский
+              </option>
             </select>
             <button className="netflix-header__button">Sign In</button>
           </div>
@@ -30,7 +49,7 @@ function Netflix() {
           <h2 className="netflix-section__title">
             Unlimited movies, TV <br /> shows, and more
           </h2>
-          <p className="netflix-section__text">
+          <p className="netflix-section__text-starts">
             Starts at EUR 7.99. Cancel anytime.
           </p>
           <p className="netflix-section__text">
@@ -40,10 +59,28 @@ function Netflix() {
           <div className="netflix-email">
             <input
               type="email"
-              placeholder="Email Adres"
+              placeholder="Email address"
               className="netflix-email__input"
             />
-            <buttton className="netflix-email__button">Get Started</buttton>
+            <buttton className="netflix-email__button">
+              Get Started
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  role="img"
+                  width="22"
+                  height="22"
+                  data-icon="ChevronRightStandard"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M15.5859 12L8.29303 19.2928L9.70725 20.7071L17.7072 12.7071C17.8948 12.5195 18.0001 12.2652 18.0001 12C18.0001 11.7347 17.8948 11.4804 17.7072 11.2928L9.70724 3.29285L8.29303 4.70706L15.5859 12Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </span>
+            </buttton>
           </div>
         </section>
       </div>
