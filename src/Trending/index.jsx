55275e2c489122c8e18d9268a6f9ae7be97b8f74
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-26 16:41:11 +0500

    trending component jsx

--- src/Trending/index.jsx
+++ src/Trending/index.jsx
@@ -4,7 +4,24 @@ function Trending() {
   return (
     <div className="trending">
       <div className="trending-container">
-        <div className="trending-dd"></div>
+        <header className="trending-header">
+          <div className="trending-header__dropdown">
+            <select className="trending-header__select">
+              <option className="trending-header__select-text" value="English">
+                Movies - Other Languages
+              </option>
+              <option className="trending-header__select-text" value="Русский">
+                TV Shows - English
+              </option>
+              <option className="trending-header__select-text" value="Русский">
+                TV Shows - Other Languages
+              </option>
+            </select>
+          </div>
+        </header>
+        <nav className="trending-navigation">
+          <div className="trending-navigation__cards"></div>
+        </nav>
       </div>
     </div>
   );
