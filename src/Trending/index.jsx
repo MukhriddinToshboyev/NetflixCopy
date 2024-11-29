9f1788d4a86c52c9e06fd329cfa1238c2662af04
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-29 23:47:31 +0500

    new component Card

--- src/Trending/index.jsx
+++ src/Trending/index.jsx
@@ -88,20 +88,6 @@ function Trending() {
             </ul>
           </div>
         </nav>
-        <section className="trending-section">
-          <div className="trending-section_text">
-            <h2>More Reasons to Join </h2>
-          </div>
-
-          <div className="trending-card">
-            <ul className="card-items">
-              <li className="card-item"></li>
-              <li className="card-item"></li>
-              <li className="card-item"></li>
-              <li className="card-item"></li>
-            </ul>
-          </div>
-        </section>
       </div>
     </div>
   );
