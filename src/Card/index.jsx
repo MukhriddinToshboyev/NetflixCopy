c186585f7c3f37b491043d13b87e4d4cfe8d23ed
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-29 23:56:21 +0500

    card jsx new teg

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -2,20 +2,28 @@ import "./Card.css";
 
 function Card() {
   return (
-    <section className="-section">
-      <div className="trending-section_text">
-        <h2 className="cars-item__text">More Reasons to Join </h2>
-      </div>
+    <div className="card-container">
+      <section className="card-section">
+        <div className="card-section_text">
+          <h2 className="cars-item__text">More Reasons to Join </h2>
+        </div>
 
-      <div className="trending-card">
-        <ul className="card-items">
-          <li className="card-item"></li>
-          <li className="card-item"></li>
-          <li className="card-item"></li>
-          <li className="card-item"></li>
-        </ul>
-      </div>
-    </section>
+        <div className="card">
+          <ul className="card-items">
+            <li className="card-item">
+              <h2>Enjoy on your TV</h2>
+              <p>
+                Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+                Blu-ray players, and more.
+              </p>
+            </li>
+            <li className="card-item"></li>
+            <li className="card-item"></li>
+            <li className="card-item"></li>
+          </ul>
+        </div>
+      </section>
+    </div>
   );
 }
 
